e9e01594100a1784159aa5d3741a5d0b9e10b74a
sharon <martinezsharonr@gmail.com>
2021-02-21 18:47:23 -0800

    added code into pseudocode, hour colors are still not working

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,66 @@
 // used moment(); function to capture todays info
-var todaysDate = moment();
+var today = moment();
 //variables made for html elements
 var hour9 = $('#9am');
 var hour10 = $('#10am');
 var hour11 = $('#11am');
 var hour12 = $('#12pm');
-var hour1 = $('#1pm');
-var hour2 = $('#2pm');
-var hour3 = $('#3pm');
-var hour4 = $('#4pm');
-var hour5 = $('#5pm');
+var hour13 = $('#1pm');
+var hour14 = $('#2pm');
+var hour15 = $('#3pm');
+var hour16 = $('#4pm');
+var hour17 = $('#5pm');
 
 //todays moment is displayed on the id tag currentDay from html and displayed on screen
-$("#currentDay").text(todaysDate.format('[Hello, today is ]dddd, MMMM Do')); 
+$("#currentDay").text(today.format('[Hello, today is ]dddd, MMMM Do')); 
 
-$("textarea" ).addClass( "present");
 
-//if the curernt time matches one of the hours on the scren change color to green
-// function pastPresentFuture() {
 
-// }
+// //if the curernt time matches one of the hours on the screen change color to green
+  function pastPresentFuture() {
+    var time = moment().format('h');
+    var hours = JSON.parse(hours)
 
-// if(currentHour === hour) {
-//   hourEl.attr('class', 'present');
+
+   if (currentHour === hours) {
+    $("textarea").addClass("present");
+  } else if (currentHour < hours) {
+    $("textarea").addClass("past");
+  } else {
+  $("textarea").addClass("future");
+  }
+  }
+  pastPresentFuture();
+ 
+  // Selects the current HTML element
+  // $("button").click(function(){
+  //   $(this).hide();
+
+ 
+//   function pastPresentFuture() 
+// //PSEUDOCODE FOR FIRST FUNCTION 
+  //   $(this).hide();
+  //for local storage function
+  // $("button").click({
+  //   mouseenter: function(){
+  //     $(this).css("background-color", "lightgray");
+  //   },
+  //   mouseleave: function(){
+  //     $(this).css("background-color", "lightblue");
+  //   },
+  //   click: function(){
+  //     $(this).css("background-color", "yellow");
+  //   }
+  // });
+
+
+// $("button").click(function(){
+//   var userInput = {
+
+//   }
+// });
+// localStorage.setItem("userInput", JSON.stringify(userInput));
+
+
+
   
-
-// }
