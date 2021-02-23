12091f39d074c1e5132f1bf9db7a8f0c67a2203e
sharon <martinezsharonr@gmail.com>
2021-02-22 19:28:35 -0800

    added variable and selector for first function and it's now working

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 // used moment(); function to capture todays info
-var today = moment();
+var time = moment();
 //variables made for html elements
 var hour9 = $('#9am');
 var hour10 = $('#10am');
@@ -11,42 +11,31 @@ var hour15 = $('#3pm');
 var hour16 = $('#4pm');
 var hour17 = $('#5pm');
 
-//todays moment is displayed on the id tag currentDay from html and displayed on screen
-$("#currentDay").text(today.format('[Hello, today is ]dddd, MMMM Do')); 
+// when window opens todays moment is displayed on the id tag currentDay from html and displayed on screen
+$("#currentDay").text(time.format('[Hello, today is ]dddd, MMMM Do')); 
 
-   //PSEUDOCODE FOR FIRST FUNCTION 
-
-//   function pastPresentFuture() {
-//     // set variables 
-//     var time = moment().format('h');
-//     var hour = JSON.parse(today.hours);
-//     console.log(time)
-//     // if, current time matches the hour on site add class .present
-
-//   if (time === hour) {
-//     $("textarea").addClass("present");
-//     //else if, the time is less than the hour add class .past
-//   } else if (time < hour) {
-//     $("textarea").addClass("past");
-//     // else set the class to .future
-//   } else {
-//     $("textarea").addClass("future");
-//   }
-// } // call on function when page loads
-// pastPresentFuture();
+  //  PSEUDOCODE FOR FIRST FUNCTION 
+function colorBasedOnHour() {
+    // set variables 
+    var hour = time.hours();
+    // selected time block class to iterate over 
+$(".time-block").each(function() {
+    // currentHour is getting the number integer from timeblocks id's
+    var currentHour = parseInt($(this).attr("id"));
+    // if, current time matches the hour on site add class .present
+    if (currentHour === hour) {
+    $("textarea").addClass("present");
+    //else if, the time is less than the hour add class .past
+  } else if (currentHour < hour) {
+    $("textarea").addClass("past");
+    // else set the class to .future
+  } else {
+    $("textarea").addClass("future");
+  }
+})
+} // call on function when page loads
+colorBasedOnHour();
  
-
-
-// //PSEUDOCODE FOR THIRDFUNCTION 
-// renderPlanner();
-function renderPlanner() {
-//   var userInput = localStorage.getItem("textarea");
-// }
- // // Retrieve
-// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-  // Use .setItem() to store object in storage
-  // JSON.stringify to convert it as a string
-// localStorage.setItem("userInput", JSON.stringify(userInput));
 
 
 // // //PSEUDOCODE FOR SECOND FUNCTION 
@@ -55,10 +44,22 @@ function renderPlanner() {
   $(".saveBtn").on("click", function(event) {
     event.preventDefault();
   // get userInput from this buttons siblings using id's or classes 
-  // var time = $(this).parent().attr("id");
   var userInput = $(this).siblings(".description").val();
-  var time = $(this).parent("div").attr("id");
-  // saves whats in textarea to local storage
+  var time = $(this).parent().attr("id");
+  // saves whats in userInput to local storage
   localStorage.setItem(time, userInput);
-}) 
+});
 
+
+
+
+// // //PSEUDOCODE FOR THIRDFUNCTION 
+// // renderPlanner();
+// function renderPlanner() {
+//   var userInput = localStorage.getItem("userInput");
+// // }
+//  // // Retrieve
+// // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+//   // Use .setItem() to store object in storage
+//   // JSON.stringify to convert it as a string
+// // localStorage.setItem("userInput", JSON.stringify(userInput));
