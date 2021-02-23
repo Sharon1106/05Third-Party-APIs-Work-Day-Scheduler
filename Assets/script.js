1cc3dca82991695d266604622b9caf84fa9912d9
sharon <martinezsharonr@gmail.com>
2021-02-22 18:29:40 -0800

    input saves to local storage now but most inputs result undefined

--- Assets/script.js
+++ Assets/script.js
@@ -16,45 +16,49 @@ $("#currentDay").text(today.format('[Hello, today is ]dddd, MMMM Do'));
 
    //PSEUDOCODE FOR FIRST FUNCTION 
 
-  function pastPresentFuture() {
-    // set variables 
-    var time = moment().format('h');
-    var hour = JSON.parse(today.hours);
-    // if, current time matches the hour on site add class .present
-  if (time === hour) {
-    $("textarea").addClass("present");
-    //else if, the time is less than the hour add class .past
-  } else if (time < hour) {
-    $("textarea").addClass("past");
-    // else set the class to .future
-  } else {
-    $("textarea").addClass("future");
-  }
-} // call on function when page loads
-pastPresentFuture();
- 
- 
-// //PSEUDOCODE FOR SECOND FUNCTION 
+//   function pastPresentFuture() {
+//     // set variables 
+//     var time = moment().format('h');
+//     var hour = JSON.parse(today.hours);
+//     console.log(time)
+//     // if, current time matches the hour on site add class .present
 
-// only when save button is clicked 
-$("button").click(function(){
-  // userInput is the text entered in textarea
-  var userInput = $("textarea").val();
-  // 
-  localStorage.setItem("textarea", userInput);
-}) 
-console.log("textarea");
+//   if (time === hour) {
+//     $("textarea").addClass("present");
+//     //else if, the time is less than the hour add class .past
+//   } else if (time < hour) {
+//     $("textarea").addClass("past");
+//     // else set the class to .future
+//   } else {
+//     $("textarea").addClass("future");
+//   }
+// } // call on function when page loads
+// pastPresentFuture();
+ 
 
-// // Retrieve
+
+// //PSEUDOCODE FOR THIRDFUNCTION 
+// renderPlanner();
+function renderPlanner() {
+//   var userInput = localStorage.getItem("textarea");
+// }
+ // // Retrieve
 // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
   // Use .setItem() to store object in storage
   // JSON.stringify to convert it as a string
 // localStorage.setItem("userInput", JSON.stringify(userInput));
 
-// //PSEUDOCODE FOR THIRDFUNCTION 
-renderPlanner();
-function renderPlanner() {
-  var userInput = localStorage.getItem("textarea");
-}
 
-  
+// // //PSEUDOCODE FOR SECOND FUNCTION 
+
+  // and button is clicked
+  $(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+  // get userInput from this buttons siblings using id's or classes 
+  // var time = $(this).parent().attr("id");
+  var userInput = $(this).siblings(".description").val();
+  var time = $(this).parent("div").attr("id");
+  // saves whats in textarea to local storage
+  localStorage.setItem(time, userInput);
+}) 
+
